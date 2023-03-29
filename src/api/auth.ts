888f2eb3cbe15201af5df8abfe83db008226c30f
Bluejuest <1960960513@qq.com>
2023-03-29 23:32:32 +0800

    变更

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,12 +3,7 @@ import request from "@/request/request";
 import { LoginAPIReq, LoginAPIRes } from "@/types/Common/auth";
 
 // 验证码请求
-export function CaptchaAPI() {
-    return request.get("/v1/captcha");
-}
+export const CaptchaAPI = (): Promise<string> => request.get("/v1/captcha");
 
 // 登录请求
-// export const LoginAPI = (params: LoginAPIReq): Promise<LoginAPIRes> => request.post("/v1/login", params);
-export function LoginAPI(params: LoginAPIReq) {
-    return request.post("/v1/login", params);
-}
+export const LoginAPI = (params: LoginAPIReq): Promise<LoginAPIRes> => request.post("/v1/login", params);
